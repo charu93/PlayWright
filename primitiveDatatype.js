147f64b9bce3dc4580f9b9b0f4f2074efcf09b94
charu93 <74642958+charu93@users.noreply.github.com>
2025-08-16 13:19:55 +0530

    Add files via upload
    added primitive Data types

---
+++ primitiveDatatype.js
@@ -0,0 +1,20 @@
+let firstName="Charmila"
+console.log(typeof firstName)
+console.log( firstName)
+let companyName="TestLeaf"
+console.log(typeof companyName)
+console.log( companyName)
+let mobileNumber=7708583705
+console.log(typeof mobileNumber)
+console.log( mobileNumber)
+let isAutomation=true
+console.log(typeof isAutomation)
+console.log( isAutomation)
+let hasPlaywright
+console.log(typeof hasPlaywright)
+console.log( hasPlaywright)
+var transactionId=null
+console .log(typeof transactionId)
+let boswerName="chrome"
+let boswerName="chrome"
+console.log(boswerName)
